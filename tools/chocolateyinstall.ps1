3ee456b2abc8b3cef98be994b46d87282fe240cf
Romain Chardiny <romain.chardiny@gmail.com>
2020-09-09 03:52:31 +0200

    New version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://education.ti.com/download/en/ed-tech/13312F7CEC074A2DAFD7EE5646129839/F3101FA52CF64929B219B7179673CDA1/TIConnectCE-5.4.0.1181.msi'
+$url        = 'https://education.ti.com/download/en/ed-tech/13312F7CEC074A2DAFD7EE5646129839/2A3ECD462CC540A1AA2838B4A7AED9A7/TIConnectCE-5.6.0.2082.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'ti-connect-ce*'
 
-  checksum      = 'A0F9CD88E2E1B4C914C5E107D7ED9490A7CD287E3E47DE4DD93B8D2DA060BA7F'
+  checksum      = '0EF703C9827EFCA78C968F16886D39D25508A31EA6B40A094464304977EFC26A'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
